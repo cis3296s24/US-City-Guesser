c6fc4511f314c0b53a56ad9eb465e9832907d52c
Dean Roos <tug91912@temple.edu>
2024-03-26 09:33:28 -0400

    Deployed site of table of cities

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
 import React, { useEffect, useState } from 'react'
-import Cities from './cities.json'
+import Cities from './city_data.json'
 
 function App() {
 
-  let total = 0
-
-  Cities.forEach((city) => total+=city.number)
-  
   return (
     <div>
-      {Cities.map(city => {
-        return (
-          <div className='box'>
-            <p>{city.city}</p>
-            <p>{city.state}</p>
-            {total}
-          </div>
-        )
-      })
-    }
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>city</th>
+            <th>state</th>
+            <th>latitude</th>
+            <th>longitude</th>
+            <th>population</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+          Cities.map((city) => 
+          <tr key = {city.id}>
+            <td>{city.id}</td>
+            <td>{city.city}</td>
+            <td>{city.state_id}</td>
+            <td>{city.lat}</td>
+            <td>{city.lng}</td>
+            <td>{city.population}</td>
+          </tr>
+          )
+          }
+        </tbody>
+      </table>
     </div>
     )
 }
