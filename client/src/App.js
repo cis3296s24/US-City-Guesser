151a8eb7931d7e167ac73a52d7fb19e69cc8d755
Dean Roos <tug91912@temple.edu>
2024-03-27 09:58:15 -0400

    updates app.js with template for future use

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import Cities from './city_data.json'
-
+import CreateTable from "./CreateTable"
+import Cities from "./city_data.json"
 function App() {
 
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>city</th>
-            <th>state</th>
-            <th>latitude</th>
-            <th>longitude</th>
-            <th>population</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-          Cities.map((city) => 
-          <tr key = {city.id}>
-            <td>{city.id}</td>
-            <td>{city.city}</td>
-            <td>{city.state_id}</td>
-            <td>{city.lat}</td>
-            <td>{city.lng}</td>
-            <td>{city.population}</td>
-          </tr>
-          )
-          }
-        </tbody>
-      </table>
+     
+      <CreateTable></CreateTable> {/* This is just for testing purposes*/}
+
+      {/* Component to generate target city */}
+
+      {/* Map component - will later include projecting guesses onto map */}
+
+      {/* Component to handle guesses (dropdown that you click, not type and enter) */}
+
+      {/* Component that takes guess from above and calculates distance */}
+
+      {/* Component that displays previous guesses in order of proximity */}
+
+
+
     </div>
     )
 }
