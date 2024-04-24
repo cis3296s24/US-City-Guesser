b30f125572d32d684e39ab2b4e82c20eab0a69bc
Dean Roos <tug91912@temple.edu>
2024-04-24 08:54:51 -0400

    Fixed bug in stats

--- src/ShowStats.js
+++ src/ShowStats.js
@@ -9,7 +9,7 @@ export default function ShowStats(difficulty){
         Number of plays: {parseInt(localStorage.getItem("easyGameCount") ?? 0)} <br/>
         Number of wins: {parseInt(localStorage.getItem("easyWinCount") ?? 0)} <br />
         Win percentage: {Math.round((parseInt(localStorage.getItem("easyWinCount") ?? 0) / parseInt(localStorage.getItem("easyGameCount") ?? 1))* 100)}% <br />
-        Average guesses per win: {Math.round(parseInt(localStorage.getItem("easyGuessCount") ?? 0) / parseInt(localStorage.getItem("easyWinCount")) ?? 1)} <br/>
+        Average guesses per win: {Math.round(parseInt(localStorage.getItem("easyGuessCount") ?? 0) / (parseInt(localStorage.getItem("easyWinCount")) ?? 1))} <br/>
       
       </div>
     )
@@ -20,7 +20,7 @@ export default function ShowStats(difficulty){
         Number of plays: {parseInt(localStorage.getItem("mediumGameCount") ?? 0)} <br/>
         Number of wins: {parseInt(localStorage.getItem("mediumWinCount") ?? 0)} <br />
         Win percentage: {Math.round((parseInt(localStorage.getItem("mediumWinCount") ?? 0) / parseInt(localStorage.getItem("mediumGameCount") ?? 1))* 100)}% <br />
-        Average guesses per win: {Math.round(parseInt(localStorage.getItem("mediumGuessCount") ?? 0) / parseInt(localStorage.getItem("mediumWinCount")) ?? 1)} <br/>
+        Average guesses per win: {Math.round(parseInt(localStorage.getItem("mediumGuessCount") ?? 0) / (parseInt(localStorage.getItem("mediumWinCount")) ?? 1))} <br/>
       
       </div>
     )
@@ -31,7 +31,7 @@ export default function ShowStats(difficulty){
         Number of plays: {parseInt(localStorage.getItem("hardGameCount") ?? 0)} <br/>
         Number of wins: {parseInt(localStorage.getItem("hardWinCount") ?? 0)} <br />
         Win percentage: {Math.round((parseInt(localStorage.getItem("hardWinCount") ?? 0) / parseInt(localStorage.getItem("hardGameCount") ?? 1))* 100)}% <br />
-        Average guesses per win: {Math.round(parseInt(localStorage.getItem("hardGuessCount") ?? 0) / parseInt(localStorage.getItem("hardWinCount")) ?? 1)} <br/>
+        Average guesses per win: {Math.round(parseInt(localStorage.getItem("hardGuessCount") ?? 0) / (parseInt(localStorage.getItem("hardWinCount")) ?? 1))} <br/>
       </div>
     )
   } else {
@@ -41,7 +41,7 @@ export default function ShowStats(difficulty){
         Number of plays: {parseInt(localStorage.getItem("impossibleGameCount") ?? 0)} <br/>
         Number of wins: {parseInt(localStorage.getItem("impossibleWinCount") ?? 0)} <br />
         Win percentage: {Math.round((parseInt(localStorage.getItem("impossibleWinCount") ?? 0) / parseInt(localStorage.getItem("impossibleGameCount") ?? 1))* 100)}% <br />
-        Average guesses per win: {Math.round(parseInt(localStorage.getItem("impossibleGuessCount") ?? 0) / parseInt(localStorage.getItem("impossibleWinCount") ?? 1))} <br/>
+        Average guesses per win: {Math.round(parseInt(localStorage.getItem("impossibleGuessCount") ?? 0) / (parseInt(localStorage.getItem("impossibleWinCount") ?? 1)))} <br/>
       
       </div>
     )
